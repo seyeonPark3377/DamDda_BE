2454e1cec897198d2f208e9ae3f8a0590400e9fe
minHee167 <167487878+mhee167@users.noreply.github.com>
2024-10-21 14:12:14 +0900

    chore : server.client.base-url -> server.client.base-url2로 변경

--- src/main/java/org/eightbit/damdda/order/controller/PaymentController.java
+++ src/main/java/org/eightbit/damdda/order/controller/PaymentController.java
@@ -24,7 +24,7 @@ public class PaymentController {
     private final KakaoPayService kakaoPayService;
     private final TossPayService tossPayService;
 
-    @Value("${server.client.base-url}")
+    @Value("${server.client.base-url2}")
     private String clientBaseUrl;
 
     @GetMapping("/toss/success")
